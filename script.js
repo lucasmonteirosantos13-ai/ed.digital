0f0a2a96ac19d3901a3b4112d7f4f1b1ff87d8c9
lucasmonteirosantos13-ai <lucas.monteiro.santos13@escola.pr.gov.br>
2026-09-23 14:37:02 -0300

    atualizado

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const botoeCurtir = document.querySelectorALL(".curtir");
+botoesCurtir.forEach(function(botaoCurtir){
+     let curtiu = false;
+     botaoCurtir.addEventListenser("Click", curtir);
+function curtir(){
+     const contador = botaoCurtir.querySelector("span");
+     if(curtiu === false){
+        contador.textContent++;
+        curtiu = true}
+        else{
+            contador.textContent--;
+            curtiu = false
+        }
 
+        }
+     });
